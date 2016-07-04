8964ce9fb208ec1cefb3a4e953db79095ac2e5e4
Alan Pulliam <Alan Pulliam>
2016-07-03 20:59:46 -0400

    Update SQL Alway On to latest DSC scripts and updated Deploy-AzureResourceGroup.ps1 to latest VS version

--- Scripts/deployCoreAzureBlob.ps1
+++ Scripts/deployCoreAzureBlob.ps1
@@ -1,7 +1,6 @@
 ﻿$resourceGroupName="apulliam-core"
 $resourceGroupLocation = "East US"
-$storageAccountResourceGroupName = "apulliam-armdeploy"
 $storageAccountName = "apulliamarmdeploy"
 $storageContainerName = $resourceGroupName + "-stageartifacts"
- 
-.\Deploy-AzureResourceGroup.ps1 -ResourceGroupLocation $resourceGroupLocation -ResourceGroupName $resourceGroupName -UploadArtifacts -StorageAccountResourceGroupName $storageAccountResourceGroupName -StorageContainerName $storageContainerName -StorageAccountName $storageAccountName -TemplateFile ..\Templates\coreDeploy.json -TemplateParametersFile ..\Templates\coreDeploy.parameters.json -ArtifactStagingDirectory "..\" -AzCopyPath ..\Tools\AzCopy.exe -DSCSourceFolder ..\DSC -Verbose 
+
+.\Deploy-AzureResourceGroup.ps1 -ResourceGroupLocation $resourceGroupLocation -ResourceGroupName $resourceGroupName -UploadArtifacts -StorageContainerName $storageContainerName -StorageAccountName $storageAccountName -TemplateFile ..\Templates\coreDeploy.json -TemplateParametersFile ..\Templates\coreDeploy.parameters.json -ArtifactStagingDirectory "..\" -DSCSourceFolder ..\DSC -Verbose 
