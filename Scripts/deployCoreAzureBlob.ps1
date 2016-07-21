bfc0b9a97f81c957464a7cafb9035e7c5221ce55
Alan Pulliam <Alan Pulliam>
2016-07-21 16:59:49 -0700

    updated SQL AOAG to match latest version from SQL team

--- Scripts/deployCoreAzureBlob.ps1
+++ Scripts/deployCoreAzureBlob.ps1
@@ -3,4 +3,4 @@ $resourceGroupLocation = "East US"
 $storageAccountName = "apulliamarmdeploy"
 $storageContainerName = $resourceGroupName + "-stageartifacts"
 
-.\Deploy-AzureResourceGroup.ps1 -ResourceGroupLocation $resourceGroupLocation -ResourceGroupName $resourceGroupName -UploadArtifacts -StorageContainerName $storageContainerName -StorageAccountName $storageAccountName -TemplateFile ..\Templates\coreDeploy.json -TemplateParametersFile ..\Templates\coreDeploy.parameters.json -ArtifactStagingDirectory "..\" -DSCSourceFolder ..\DSC -Verbose 
+.\Deploy-AzureResourceGroup.ps1 -ResourceGroupLocation $resourceGroupLocation -ResourceGroupName $resourceGroupName -UploadArtifacts -StorageContainerName $storageContainerName -StorageAccountName $storageAccountName -TemplateFile ..\Templates\coreDeploy.json -TemplateParametersFile ..\Templates\coreDeploy.parameters.json -ArtifactStagingDirectory ".." -DSCSourceFolder ..\DSC -Verbose 
